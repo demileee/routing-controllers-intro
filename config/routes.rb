60f81260ce08ff83235c32ce36351d4e17c13deb
Demi Lee <demilee.to@gmail.com>
2017-10-11 11:08:17 -0400

    add prefixes to dynamic routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/welcome' => 'pages#welcome'
   get '/about' => 'pages#about'
   get '/contest' => 'pages#contest'
-  get '/kitten/:size' => 'pages#kitten'
-  get '/kittens/:size' => 'pages#kittens'
-  get '/secrets/:magic_word' => 'pages#secrets'
+  get '/kitten/:size' => 'pages#kitten', as: 'kitten'
+  get '/kittens/:size' => 'pages#kittens', as: 'kittens'
+  get '/secrets/:magic_word' => 'pages#secrets', as: 'secrets'
 end
