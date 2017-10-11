52b6063bc0d30d802db3e2c3439122f55fc28f81
Demi Lee <demilee.to@gmail.com>
2017-10-11 10:56:22 -0400

    add kitten and kittens route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,26 @@
 class PagesController < ApplicationController
 
-def welcome
-  @header = "This is the welcome page"
-end
+  before_action :set_kitten_url, only: [:kitten, :kittens]
 
-def about
-end
+  def welcome
+    @header = "This is the welcome page"
+  end
 
-def contest
-end
+  def about
+  end
 
+  def contest
+  end
 
+  def kitten
+  end
+
+  def kittens
+  end
+
+  def set_kitten_url
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
 
 end
